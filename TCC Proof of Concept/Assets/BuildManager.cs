3bb9ca014b78fa4c8e073e87bcc902a4471957f3
Bernardo <3ernardo@gmail.com>
2019-11-01 03:47:29 -0300

    Start screen and placement mechanic

---
+++ TCC Proof of Concept/Assets/BuildManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+    public static BuildManager instance;
+
+    void Awake() {
+        instance = this;
+    }
+
+    public GameObject standardMachinePrefab;
+
+    private void Start() {
+        machineToBuild = standardMachinePrefab;
+    }
+
+    private GameObject machineToBuild;
+
+    public GameObject GetMachineToBuild() {
+        return machineToBuild;
+    }
+
+}
---
+++ TCC Proof of Concept/Assets/Node.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+
+    private GameObject machine;
+    public Vector3 positionOffset;
+
+    public Color hoverColor;
+    private Color startColor;
+
+    private Renderer rend;
+
+    void Start() {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown() {
+        if (machine != null)
+        {
+            Debug.Log("Já existe algo construído aqui.");
+            return;
+        }
+
+        GameObject machineToBuild = BuildManager.instance.GetMachineToBuild();
+        machine = (GameObject)Instantiate(machineToBuild, transform.position + positionOffset, transform.rotation);
+
+    }
+
+    void OnMouseEnter() {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit() {
+        rend.material.color = startColor;
+    }
+}
