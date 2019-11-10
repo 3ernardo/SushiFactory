17b6d88faa77b33980d90b3135126ffea89370c2
Bernardo <3ernardo@gmail.com>
2019-11-10 13:00:09 -0300

    Added delete item mechanics in machines

---
+++ TCC Proof of Concept/Assets/Machine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machine : MonoBehaviour
+{
+
+    // private void OnCollisionEnter(Collision other) {
+    private void OnTriggerStay(Collider other) {
+        Debug.Log("Objeto sobre a máquina, " + other.gameObject.name);
+        Destroy(other.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
