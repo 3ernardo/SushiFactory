f0787c697e8aaf11150cf82205879bdda4770fd3
Bernardo Araujo <3ernardo@gmail.com>
2019-12-03 21:19:03 -0300

    Added timer

---
+++ TCC Proof of Concept/Assets/Scripts/Countdown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+
+    public float timer = 120f;
+    private float timeLeft;
+    public Text displayTimer;
+
+    private void counter(){
+        timeLeft -= Time.deltaTime;
+        displayTimer.text = (timeLeft).ToString("0");
+        Debug.Log(timeLeft);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter();
+    }
+}
