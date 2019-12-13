4c29b30bc6b7e733ef8c8c6ab288348f1af738d0
Bernardo Araujo <3ernardo@gmail.com>
2019-12-13 15:44:18 -0300

    Ui blocker dropdown buttons and other tweaks

--- TCC Proof of Concept/Assets/_Scripts/BuildManager.cs
+++ TCC Proof of Concept/Assets/_Scripts/BuildManager.cs
@@ -37,6 +37,11 @@ public class BuildManager : MonoBehaviour
     private Node selectedNode;
     public SelectUI selectUI;
 
+    public GameObject beltDropDown;
+    public GameObject machineDropDown;
+    public GameObject spawnDropDown;
+    public GameObject uiBlocker;
+
     public GameObject GetMachineToBuild() {
         return machineToBuild;
     }
@@ -44,6 +49,55 @@ public class BuildManager : MonoBehaviour
     public void setMachineToBuild(GameObject machine) {
         machineToBuild = machine;
         deselectNode();
+    }
+
+    // public void handleBeltDropDown(int i) {
+    //     if (i == 0) {
+    //         setMachineToBuild(conveyorUpMachinePrefab);
+    //     }
+    //     if (i == 1) {
+    //         setMachineToBuild(conveyorDownMachinePrefab);
+    //     }
+    //     if (i == 2) {
+    //         setMachineToBuild(conveyorRightMachinePrefab);
+    //     }
+    //     if (i == 3) {
+    //         setMachineToBuild(conveyorLeftMachinePrefab);
+    //     }
+    // }
+
+    public void switchButtonsBelt(){
+        if (beltDropDown.activeSelf) {
+            beltDropDown.SetActive(false);
+        } else {
+            beltDropDown.SetActive(true);
+            uiBlocker.SetActive(true);
+        }
+    }
+
+    public void switchButtonsMachine(){
+        if (machineDropDown.activeSelf) {
+            machineDropDown.SetActive(false);
+        } else {
+            machineDropDown.SetActive(true);
+            uiBlocker.SetActive(true);
+        }
+    }
+
+    public void switchButtonsSpawn(){
+        if (spawnDropDown.activeSelf) {
+            spawnDropDown.SetActive(false);
+        } else {
+            spawnDropDown.SetActive(true);
+            uiBlocker.SetActive(true);
+        }
+    }
+
+    public void hideDropDowns(){
+        beltDropDown.SetActive(false);
+        machineDropDown.SetActive(false);
+        spawnDropDown.SetActive(false);
+        uiBlocker.SetActive(false);
     }
 
     public void selectNode (Node node) {
@@ -62,7 +116,7 @@ public class BuildManager : MonoBehaviour
     }
 
     private void Start() {
-        machineToBuild = standardMachinePrefab;
+        machineToBuild = null;
         deselectNode();
         // PlayerMoney = 100f;
         PlayerWallet.instance.setMoney(PlayerMoney);
@@ -77,33 +131,43 @@ public class BuildManager : MonoBehaviour
 
     public void ChangeMachineConveyorRight() {
         setMachineToBuild(conveyorRightMachinePrefab);
+        hideDropDowns();
     }
     public void ChangeMachineConveyorUp() {
         setMachineToBuild(conveyorUpMachinePrefab);
+        hideDropDowns();
     }
     public void ChangeMachineConveyorDown() {
         setMachineToBuild(conveyorDownMachinePrefab);
+        hideDropDowns();
     }
     public void ChangeMachineConveyorLeft() {
         setMachineToBuild(conveyorLeftMachinePrefab);
+        hideDropDowns();
     }
     public void ChangeMachineStandard() {
         setMachineToBuild(standardMachinePrefab);
+        hideDropDowns();
     }
     public void ChangeMachineAdvanced() {
         setMachineToBuild(advancedMachinePrefab);
+        hideDropDowns();
     }
     public void ChangeMachineBasic() {
         setMachineToBuild(basicMachinePrefab);
+        hideDropDowns();
     }
     public void ChangeSpawnerFish() {
         setMachineToBuild(spawnerFish);
+        hideDropDowns();
     }
     public void ChangeSpawnerNori() {
         setMachineToBuild(spawnerNori);
+        hideDropDowns();
     }
     public void ChangeSpawnerRice() {
         setMachineToBuild(spawnerRice);
+        hideDropDowns();
     }
 
 }
